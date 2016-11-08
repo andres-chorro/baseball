1f32e4ac3160054543c76d3160ae7c297e6f650b
chorro73 <chorro73@gmail.com>
2016-11-08 13:08:43 -0800

    Start PlayerSerializer class and testing

---
+++ src/baseball/PlayerSerializer.java
@@ -0,0 +1,52 @@
+package baseball;
+
+import java.io.*;
+import java.util.*;
+
+public class PlayerSerializer {
+	private String filePath;
+
+	public PlayerSerializer(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public void save(List<Player> players) {
+		try {
+			FileOutputStream fileOut = new FileOutputStream(filePath);
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(players);
+			out.close();
+			fileOut.close();
+			
+		} catch (IOException i) {
+			i.printStackTrace();
+		}
+	}
+	
+	public List<Player> load() {
+		try {
+	         FileInputStream fileIn = new FileInputStream(filePath);
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         ArrayList<Player> playerList = (ArrayList<Player>) in.readObject();
+	         in.close();
+	         fileIn.close();
+	         return playerList;
+	      }catch(IOException i) {
+	         i.printStackTrace();
+	         return null;
+	      }catch(ClassNotFoundException c) {
+	         System.out.println("Player List class not found");
+	         c.printStackTrace();
+	         return null;
+	      }
+	}
+	
+	public static void main(String[] args) {
+		PlayerSerializer ps = new PlayerSerializer("test1.ser");
+//		List<Player> players = new ArrayList<>();
+//		players.add(new Player("Andres", "Chorro", 73));
+//		players.add(new Player("Quique", "Chorro", 16));
+//		ps.save(players);
+		System.out.println(ps.load());
+	}
+}

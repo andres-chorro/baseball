fcb7314760f84fc26fbb0c96029232f94806eb04
chorro73 <chorro73@gmail.com>
2016-11-16 21:57:21 -0800

    Add scores. Start Single(). Add getCurrentBatter(). Start countRuns()

--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -9,6 +9,8 @@ public class Game {
 	private int currentHomeHitter;
 	private int inning;
 	private boolean isBottom;
+	private int awayScore;
+	private int homeScore;
 	private LinkedList<Player> bases;
 
 	public Game(List<Player> awayTeam, List<Player> homeTeam) {
@@ -18,10 +20,37 @@ public class Game {
 		currentHomeHitter = 0;
 		inning = 1;
 		isBottom = false;
+		awayScore = 0;
+		homeScore = 0;
 		bases = new LinkedList<>();
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 3; i++) {
 			bases.push(null);
 		}
+	}
+
+	/**
+	 * Update the game to reflect a single by the current hitter.
+	 */
+	public void single() {
+		Player currentHitter = getCurrentBatter();
+		bases.push(currentHitter);
+		currentHitter.single(countRuns());
+	}
+	
+	/**
+	 * Checks how many runners scored on the last hit, and clears the bases structure
+	 * to keep only the current base runners. Adjusts score accordingly.
+	 * @return
+	 */
+	private int countRuns() {
+		// TODO: count runs
+		// TODO: clear bases
+		// TODO: increments scores
+		return 0;
+	}
+	
+	private Player getCurrentBatter() {
+		return isBottom ? homeTeam.get(currentHomeHitter) : awayTeam.get(currentAwayHitter);
 	}
 
 	public static void main(String[] args) {

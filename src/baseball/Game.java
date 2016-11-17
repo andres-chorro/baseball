98ecc550de4e4e7a6bfdcaff335c095e73da2da3
chorro73 <chorro73@gmail.com>
2016-11-16 21:30:52 -0800

    Generalize team type to List

--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -2,17 +2,19 @@ package baseball;
 import java.util.*;
 
 public class Game {
-	private ArrayList<Player> awayTeam;
-	private ArrayList<Player> homeTeam;
+	private List<Player> awayTeam;
+	private List<Player> homeTeam;
 	private int inning;
 	private boolean isBottom;
 	private LinkedList<Player> bases;
 	
-	public Game(ArrayList<Player> awayTeam, ArrayList<Player> homeTeam) {
+	public Game(List<Player> awayTeam, List<Player> homeTeam) {
 		this.awayTeam = awayTeam;
 		this.homeTeam = homeTeam;
+		
 		inning = 1;
 		isBottom = false;
+		
 		bases = new LinkedList<>();
 		for (int i = 0; i < 4; i++) {
 			bases.push(null);

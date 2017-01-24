329aa4e4615505066c1daa30199e5a32cc63a6ef
chorro73 <chorro73@gmail.com>
2017-01-24 13:40:27 -0800

    Add rbis test

--- src/baseball/PlayerTest.java
+++ src/baseball/PlayerTest.java
@@ -39,7 +39,12 @@ public class PlayerTest {
 		assertEquals(0, p.getHomeruns());
 		p.recordHomerun();
 		assertEquals(1, p.getHomeruns());
-		
+	}
+	
+	@Test
+	public void recordRbis() {
+		p.recordRbis(3);
+		assertEquals(3, p.getRbis());
 	}
 
 }

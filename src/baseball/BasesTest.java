1b9bd9ffd6835740aeda58a84f8cfe7c7ad667a7
chorro73 <chorro73@gmail.com>
2017-02-27 15:55:58 -0800

    Clean up Bases Tests

--- src/baseball/BasesTest.java
+++ src/baseball/BasesTest.java
@@ -37,50 +37,20 @@ public class BasesTest {
 		b.advanceRunners(2);
 		assertEquals(1, b.getNumRunnersOn());
 		assertEquals(p1, b.getRunnerOnThird());
+		assertEquals(Bases.NO_RUNNER, b.getRunnerOnFirst());
+		b.advanceRunners(1);
+		assertEquals(Bases.NO_RUNNER, b.getRunnerOnThird());
 		
 	}
 
-	@Test 
-	public void testNumRunnersOn() {
-		assertEquals(0, b.getNumRunnersOn());
-		b.putRunnerOn(p1, Base.FIRST);
-		assertEquals(1, b.getNumRunnersOn());
-		b.putRunnerOn(p2, Base.SECOND);
-		assertEquals(2, b.getNumRunnersOn());
-		b.putRunnerOn(p3, Base.THIRD);
-		assertEquals(3, b.getNumRunnersOn());
-		b.advanceRunners(3);
-		assertEquals(0, b.getNumRunnersOn());
-	}
-	
 	@Test
-	public void putRunnerOn() {
+	public void testPutRunnerOnAndGetRunners() {
 		b.putRunnerOn(p3, Base.THIRD);
 		b.putRunnerOn(p1, Base.FIRST);
 		b.putRunnerOn(p2, Base.SECOND);
 		assertEquals(p1, b.getRunnerOnFirst());
 		assertEquals(p2, b.getRunnerOnSecond());
 		assertEquals(p3, b.getRunnerOnThird());
-	}
-	
-	@Test
-	public void testGetAndClearBases() {
-		b.putRunnerOn(p1, Base.THIRD);
-		b.advanceRunners(1);
-		List<Player> scorers = b.getAndCleanRunnersIn();
-		assertEquals(1, scorers.size());
-		assertEquals(p1, scorers.get(0));
-		
-		scorers = b.getAndCleanRunnersIn();
-		assertTrue(scorers.isEmpty());
-		
-		b.putRunnerOn(p1, Base.FIRST);
-		b.putRunnerOn(p2, Base.THIRD);
-		b.advanceRunners(4);
-		scorers = b.getAndCleanRunnersIn();
-		assertEquals(2, scorers.size());
-		assertEquals(p2, scorers.get(0));
-		assertEquals(p1, scorers.get(1));
 	}
 	
 	@Test
@@ -97,6 +67,39 @@ public class BasesTest {
 		assertEquals(Bases.NO_RUNNER, b.getRunnerOnThird());
 		assertEquals(Bases.NO_RUNNER, b.getRunnerOnSecond());
 		assertEquals(p1, b.getRunnerOnFirst());
+	}
+
+	@Test 
+	public void testNumRunnersOn() {
+		assertEquals(0, b.getNumRunnersOn());
+		b.putRunnerOn(p1, Base.FIRST);
+		assertEquals(1, b.getNumRunnersOn());
+		b.putRunnerOn(p2, Base.SECOND);
+		assertEquals(2, b.getNumRunnersOn());
+		b.putRunnerOn(p3, Base.THIRD);
+		assertEquals(3, b.getNumRunnersOn());
+		b.advanceRunners(3);
+		assertEquals(0, b.getNumRunnersOn());
+	}
+
+	@Test
+	public void testGetAndCleanRunnersIn() {
+		b.putRunnerOn(p1, Base.THIRD);
+		b.advanceRunners(1);
+		List<Player> scorers = b.getAndCleanRunnersIn();
+		assertEquals(1, scorers.size());
+		assertEquals(p1, scorers.get(0));
+		
+		scorers = b.getAndCleanRunnersIn();
+		assertTrue(scorers.isEmpty());
+		
+		b.putRunnerOn(p1, Base.FIRST);
+		b.putRunnerOn(p2, Base.THIRD);
+		b.advanceRunners(4);
+		scorers = b.getAndCleanRunnersIn();
+		assertEquals(2, scorers.size());
+		assertEquals(p2, scorers.get(0));
+		assertEquals(p1, scorers.get(1));
 	}
 
 }
